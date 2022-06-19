855840dbaf6f81abfc57d75b9db9d6e0d9d08afe
Charles <cchester1@angelo.edu>
2022-06-19 08:28:08 -0500

    stylized page

--- public/javascript/dom.js
+++ public/javascript/dom.js
@@ -4,10 +4,24 @@ const printNumbers = async () => {
       const data = await fetch("http://localhost:3001/data")
       data.json().then(res => {
             Object.keys(res).forEach(( key ) => {
-                  let newItem = document.createElement('li')
-                  newItem.textContent = res[key].item
-      
-                  data_here.appendChild(newItem)
+                  let masterForItem = document.createElement('div')
+                  let itemNumber = document.createElement('h3')
+                  let itemAppearances = document.createElement('li')
+                  let itemArrays = document.createElement('li')
+                  let indexOfOne = document.createElement('li')
+                  let indexOfTwo = document.createElement('li')
+                  itemNumber.textContent = 'Item: ' + res[key].item
+                  itemAppearances.textContent = 'Number of Appearances: ' + res[key].appearances 
+                  itemArrays.textContent = 'Arrays Present: ' + res[key].arrays_present
+                  indexOfOne.textContent = 'Index Of Presence in Array One: ' + res[key].indexes.index_of_one
+                  indexOfTwo.textContent = 'Index of Presence in Array Two: ' + res[key].indexes.index_of_two
+                  
+                  itemNumber.appendChild(indexOfTwo)
+                  itemNumber.appendChild(indexOfOne)
+                  itemNumber.appendChild(itemArrays)
+                  itemNumber.appendChild(itemAppearances)
+                  masterForItem.appendChild(itemNumber)
+                  data_here.appendChild(masterForItem)
             })
       })
       
