2074a83372b87dc38c409c147f74699541ebb81e
Charles <cchester1@angelo.edu>
2022-06-23 22:49:54 -0500

    modal completed

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -2,6 +2,11 @@ const searchBtn = document.querySelector('#searchBtn')
 const searchBar = document.getElementById('searchBar')
 const searchResults = document.getElementById('searchResults')
 const itemName = document.getElementById('itemName')
+const appearances = document.getElementById('appearances')
+const arraysPresent = document.getElementById('arraysPresent')
+const indexOfOne = document.getElementById('indexOfOne')
+const indexOfTwo = document.getElementById('indexOfTwo')
+const hideResultsBtn = document.getElementById('hideResultsBtn')
 
 searchBtn.addEventListener('click', async () => {
       let text = searchBar.value
@@ -17,10 +22,24 @@ searchBtn.addEventListener('click', async () => {
                               window.alert("We could not find the number you've searched for in our database")
                               searchBar.value = ''
                         }
-                        itemName.textContent = result.item
+                        itemName.textContent = 'Item Name: ' + result.item
+                        appearances.textContent = 'Number of Appearances: ' + result.appearances
+                        arraysPresent.textContent = 'Arrays Present: ' + result.arrays_present
+                        indexOfOne.textContent = 'Index Location in Array One: ' + result.indexes.index_of_one
+                        indexOfTwo.textContent = 'Index Location in Array Two: ' + result.indexes.index_of_two
+
                         searchResults.setAttribute('style', 'display: flex')
                         searchBar.value = ''
                   }
+
+                  hideResultsBtn.addEventListener('click', () => {
+                        itemName.textContent = ''
+                        appearances.textContent = ''
+                        arraysPresent.textContent = ''
+                        indexOfOne.textContent = ''
+                        indexOfTwo.textContent = ''
+                        searchResults.setAttribute('style', 'display: none')
+                  })
 
                   findSearchRequest()
             })
