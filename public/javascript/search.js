e87ee880d5825afabce0e975b19b35042f77c46f
Charles <cchester1@angelo.edu>
2022-06-23 22:23:40 -0500

    ..

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,32 +1,28 @@
-const searchBtn= document.querySelector('#searchBtn')
+const searchBtn = document.querySelector('#searchBtn')
 const searchBar = document.getElementById('searchBar')
 const searchResults = document.getElementById('searchResults')
+const itemName = document.getElementById('itemName')
 
 searchBtn.addEventListener('click', async () => {
       let text = searchBar.value
       const data = await fetch('http://localhost:3001/data')
 
       data.json()
-      .then(res => {
-            const database = Object.entries(res) 
+            .then(res => {
+                  const database = res
 
-            const findSearchRequest = function () {
-                  for(let i = 0; i < database.length; i ++) {
-                        if(database[i][0] === text) {
-                              const results = JSON.stringify(database[i])
-                              searchResults.textContent = results
-
+                  const findSearchRequest = function () {
+                        const result = database[text];
+                        if (!result) {
+                              window.alert("We could not find the number you've searched for in our database")
                               searchBar.value = ''
-
-                              return
                         }
+                        itemName.textContent = result.item
+                        searchResults.setAttribute('style', 'display: flex')
+                        searchBar.value = ''
                   }
 
-                  window.alert("We could not find the number you've searched for in our database")
-                  searchBar.value = ''
-            }
-
-            findSearchRequest()
-      })
+                  findSearchRequest()
+            })
 })
 
