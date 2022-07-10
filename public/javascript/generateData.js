c71ff916af7d0b7e122b769b48bc7e440b0ecd7b
Charles <cchester1@angelo.edu>
2022-07-10 13:41:56 -0500

    ..

--- public/javascript/generateData.js
+++ public/javascript/generateData.js
@@ -1,8 +1,7 @@
 // this file just needs to reinitialize the function which creates our json
 // then redirect us to the index page which will take care of the rest
 const generateDataBtn = document.querySelector('#generateResults')
-const writeJSON = require('../../writeJSON')
+
 generateDataBtn.addEventListener('click', () => {
-      writeJSON()
       document.location.replace('/')
 })
