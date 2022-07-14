be660b262c0939f716248bc9a9bb4cf750485243
unknown <cchester1@angelo.edu>
2022-07-14 12:33:36 -0500

    working with generate data feature

--- public/javascript/generateData.js
+++ public/javascript/generateData.js
@@ -1,7 +1,27 @@
 // this file just needs to reinitialize the function which creates our json
 // then redirect us to the index page which will take care of the rest
 const generateDataBtn = document.querySelector('#generateResults')
+const controller = new AbortController()
+const signal = controller.signal
+
+// function abortFetching() {
+//       console.log('Now aborting');
+//       // Abort.
+//       controller.abort()
+// }
 
 generateDataBtn.addEventListener('click', () => {
-      document.location.replace('/')
+      fetch('localhost:3001/generate', {
+            method: 'get',
+            signal: signal
+      })
+      .then(() => {
+            document.location.replace('/')
+            controller.abort()
+      })
+      .catch(err => {
+            if(err) {
+                  throw new Error(err)
+            }
+      })
 })
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const data = require('./data/index.json')
+const writeJSON = require('./scripts/writeJSON')
 
 const PORT = 3001
 
@@ -12,6 +13,11 @@ app.get('/home', (req, res) => {
 })
 app.get('/data', (req, res) => {
       res.json(data)
+})
+app.get('/generate', (req, res) => {
+      writeJSON().then(() => {
+            res.json(data)
+      })
 })
 
 app.listen(PORT, (err) => {
