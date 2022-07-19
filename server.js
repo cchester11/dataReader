19b474344506050d2c633f36416840c43b6b09e9
Charles <cchester1@angelo.edu>
2022-07-18 22:12:23 -0500

    ..

--- server.js
+++ server.js
@@ -25,23 +25,23 @@ app.get('/data', (req, res) => {
       res.json(data)
 })
 
+// if data changes restart the server
+watcher.on('change', (path) => { log(`File ${path} has been changed`)})
+watcher.on('change', () => {
+      server.close()
+      app.listen(PORT, (err) => {
+            if(err) {
+                  throw new Error(err)
+            } else {
+                  console.log('Listening on PORT ' + PORT)
+            }
+      })
+})
+
 const server = app.listen(PORT, (err) => {
       if(err) {
             throw new Error(err)
       } else {
             console.log('Listening on PORT ' + PORT)
       }
-})
-
-// if data changes restart the server
-watcher.on('change', (path) => { log(`File ${path} has been changed`)})
-watcher.on('change', () => {
-      server.close()
-      app.listen(PORT, err => {
-            if(err) {
-                  throw new Error(err)
-            } else {
-                  log('server restarted. Listening on PORT ' + PORT)
-            }
-      })
 })
