fc572311f8f0c5ecb7a11b8bbb7a187e71494250
Charles <cchester1@angelo.edu>
2022-07-23 16:30:39 -0500

    ..

--- public/javascript/generateData.js
+++ public/javascript/generateData.js
@@ -1,20 +1,14 @@
 const controller = new AbortController()
 const signal = controller.signal
 
-// function abortFetching() {
-//       console.log('Now aborting');
-//       // Abort.
-//       controller.abort()
-// }
-
 const generateDataBtn = async () => {
       await fetch('http://localhost:3001/api/generate', {
             method:'get',
             signal: signal
       })
       .then(() => {
-            document.location.replace('/')
             controller.abort()
+            document.location.replace('/')
       })
       .catch(err => {
             if(err) {
--- server.js
+++ server.js
@@ -11,28 +11,28 @@ const log = console.log.bind(console)
 const PORT = 3001
 
 app.use(require('./routes'))
-
 app.use(express.static(path.join(__dirname, 'public')))
 
 // if data changes restart the server
 watcher.on('change', (path) => { log(`File ${path} has been changed`)})
-watcher.on('change', () => {
-      server.close(() => {
-            const server = express()
-            console.info('Server closed. Restarting..')
+// watcher.on('change', () => {
+//       server.close(() => {
+//             const server = express()
+//             console.info('Server closed. Restarting..')
 
-            server.get('/', (req, res) => {
-                  res.sendFile(path.join(__dirname, './public/index.html'))
-            })
-            server.listen(PORT, (err) => {
-                  if(err) {
-                        throw new Error(err)
-                  } else {
-                        console.log('Listening on PORT ' + PORT)
-                  }
-            })
-      })
-})
+//             server.get('/', (req, res) => {
+//                   res.sendFile(path.join(__dirname, './public/index.html'))
+//                   console.log('index.html sent from watcher callback')
+//             })
+//             server.listen(PORT, (err) => {
+//                   if(err) {
+//                         throw new Error(err)
+//                   } else {
+//                         console.log('Listening on PORT ' + PORT)
+//                   }
+//             })
+//       })
+// })
 
 const server = app.listen(PORT, (err) => {
       if(err) {
