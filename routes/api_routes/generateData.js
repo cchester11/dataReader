63d8973a0c9240ad6c1f0d0fbfc1eefd828e38a2
Charles <cchester1@angelo.edu>
2022-10-22 00:37:56 -0500

    #5 minor change

--- routes/api_routes/generateData.js
+++ routes/api_routes/generateData.js
@@ -5,7 +5,7 @@ const { data } = require('../../data/index.json')
 
 router.get('/generate', (req, res) => {
       // const data = mapPattern()
-      // res.send(map)
+      // res.send(data)
       mapPattern()
       res.json(data)
 })
